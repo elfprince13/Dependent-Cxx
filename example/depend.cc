4224b087042ee391fb112edd8e9a440818833db6
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2021-10-21 20:20:56 -0400

    this is actually mostly working, in concept. need more algebra rules to get my proof terms

--- example/depend.cc
+++ example/depend.cc
@@ -1,6 +1,7 @@
 #include <array>
 #include <iostream>
 #include <optional>
+//#include <pair>
 #include <string_view>
 #include <tuple>
 #include <typeinfo>
@@ -10,27 +11,34 @@
 
 #include <boost/preprocessor/stringize.hpp>
 
-template<char ...Cs> struct CharList {};
-template<class CL, class ...TPs>
-struct TypeParamCharList {
-	template<size_t ...VPs>
-	struct ValueParamCharList {};
-};
+namespace detail {
+	template<size_t ...Nums>
+	struct Unique {};
 
-template<char ...Cs>
-std::ostream& operator<<(std::ostream& out, CharList<Cs...>) {
-	constexpr static const std::array<const char, sizeof...(Cs)> data{Cs...};
-	static_assert(data.back() == 0, "Must be null terminated");
-	return (out << data.data());
-}
+	/*
+	template<class CL, class ...TPs>
+	struct TypeParamCharList {
+		template<size_t ...VPs>
+		struct ValueParamCharList {};
+	};
+	*/
 
-template<class CL, class ...TPs, size_t ...VPs>
-std::ostream& operator<<(std::ostream& out, typename TypeParamCharList<CL, TPs...>::template ValueParamCharList<VPs...>) {
-	out << "<" << CL{};
-	(... , (out << ", " << typeid(TPs).name()));
-	out << ">::<";
-	(... , (out << VPs << ","));
-	return out << ">";
+	template<size_t ...Nums>
+	std::ostream& operator<<(std::ostream& out, Unique<Nums...>) {
+		out <<"Unique<";
+		(..., (out << Nums << ","));
+		return out << ">";
+	}
+	/*
+	template<class CL, class ...TPs, size_t ...VPs>
+	std::ostream& operator<<(std::ostream& out, typename TypeParamCharList<CL, TPs...>::template ValueParamCharList<VPs...>) {
+		out << "<" << CL{};
+		(... , (out << ", " << typeid(TPs).name()));
+		out << ">::<";
+		(... , (out << VPs << ","));
+		return out << ">";
+	}
+	 */
 }
 
 template<class Root, class ...Trail> struct ContextTag {};
@@ -64,6 +72,59 @@ std::ostream& operator<<(std::ostream& out, ContextTag<Root, Trail...>) {
 	return out << ">";
 }
 
+template<class Tag, class V> struct Fresh : Tag {
+	const V v;
+};
+
+template<class Tag, class V> Fresh(Tag, V) -> Fresh<Tag, V>;
+
+template<class C>
+struct is_fresh { constexpr static const bool value = false; };
+template<class Tag, class V>
+struct is_fresh<Fresh<Tag,V>> { constexpr static const bool value = true; };
+
+template<auto V> struct Constant {
+	const decltype(V) v;
+};
+
+template<class C>
+struct is_constant { constexpr static const bool value = false; };
+template<auto V>
+struct is_constant<Constant<V>> { constexpr static const bool value = true; };
+
+template<class Tag, class V>
+std::ostream& operator<<(std::ostream& out, const Fresh<Tag, V>& fresh) {
+	return out << "Fresh{" << (Tag)fresh << ", " << fresh.v << "}";
+}
+
+template<auto V>
+std::ostream& operator<<(std::ostream& out, const Constant<V>) {
+	return out << "Constant{" << V << "}";
+}
+
+template<class Validator, class Term>
+class CanValidate {
+	static_assert(detail::is_context_tag<Validator>::value, "Validator must be a context tag");
+	static_assert(is_fresh<Term>::value || is_constant<Term>::value, "Term must be a context tag");
+};
+
+template<class Validator, auto V>
+class CanValidate<Validator, Constant<V>> {
+public:
+	constexpr static const bool value = true;
+};
+
+
+template<class RootV, class ...TrailV,
+         class RootT, class ...TrailT, class TV>
+class CanValidate<ContextTag<RootV, TrailV...>, Fresh<ContextTag<RootT, TrailT...>, TV>> {
+protected:
+	constexpr static const bool SameRoot = std::is_same_v<RootV, RootT>;
+	constexpr static const bool AnyRecursion = (... || detail::is_fixed_point<TrailT>::value);
+public:
+	constexpr static const bool value = SameRoot && !AnyRecursion;
+};
+
 template<class CompareContext, class Left, class Right>
 struct Equivalent {
 	static_assert(detail::is_context_tag<CompareContext>::value, "CompareContext must be a context tag");
@@ -84,41 +145,6 @@ public:
 	static_assert(SameRoot, "Cannot compare contexts which do not share a root and/or which are being inspected outside their root context. Consider calling RefreshType on Left and Right within the CompareContext");
 	static_assert(!AnyRecursion || !SameComps, "Left and Right tags are equivalent but contain recursive fixed points and thus cannot be guaranteed unique. Consider re-rooting in a fresh context");
 };
-
-
-namespace detail {
-	/*
-	template<size_t N> using CharArrayRef = const char(&)[N];
-	template<size_t N>
-	class String {
-		template<typename Indices, CharArrayRef<N> Literal>
-		struct Helper {};
-		template<size_t ...I, CharArrayRef<N> Literal>
-		struct Helper<std::index_sequence<I...>, Literal> {
-			using type = CharList<Literal[I]...>;
-		};
-	public:
-		template<CharArrayRef<N> Literal>
-		struct apply {
-			using type = typename Helper<std::make_index_sequence<N>, Literal>::type;
-		};
-	};
-	*/
-	
-	template<class StringHolder, size_t ...I>
-	constexpr auto StrToCharListImpl(StringHolder holder, std::index_sequence<I...>) {
-		constexpr std::string_view text{holder()};
-		return CharList<text[I] ...>();
-	}
-	
-	template<class StringHolder>
-	constexpr auto StrToCharList(StringHolder holder) {
-		return StrToCharListImpl(holder,
-								 std::make_index_sequence<std::string_view{holder()}.length() + 1>{});
-	}
-}
-
-#define StrToCharList(X) detail::StrToCharList([]() constexpr { return (X); })
 
 namespace detail {
 	
@@ -180,38 +206,6 @@ constexpr auto pushContext(CT context, P p) {
 	return typename detail::PushContext<CT, P> ::type{};
 }
 
-template<class Tag, class V> struct Fresh : Tag {
-	const V v;
-};
-
-template<class Tag, class V> Fresh(Tag, V) -> Fresh<Tag, V>;
-
-template<class C>
-struct is_fresh { constexpr static const bool value = false; };
-template<class Tag, class V>
-struct is_fresh<Fresh<Tag,V>> { constexpr static const bool value = true; };
-
-template<auto V> struct Constant {
-	const decltype(V) v;
-};
-
-template<class C>
-struct is_constant { constexpr static const bool value = false; };
-template<auto V>
-struct is_constant<Constant<V>> { constexpr static const bool value = true; };
-
-template<class Tag, class V>
-std::ostream& operator<<(std::ostream& out, const Fresh<Tag, V>& fresh) {
-	return out << "Fresh{" << (Tag)fresh << ", " << fresh.v << "}";
-}
-
-template<auto V>
-std::ostream& operator<<(std::ostream& out, const Constant<V>) {
-	return out << "Constant{" << V << "}";
-}
-
-
-
 namespace detail {
 	template<class CContext, class LContext, class LV, class RContext, class RV>
 	constexpr bool dependEquiv(Fresh<LContext,LV>, Fresh<RContext,RV>) {
@@ -221,12 +215,12 @@ namespace detail {
 
 #define DEPEND_EQUIV(Left, Right) (detail::dependEquiv<Context>(Left, Right))
 
-#define LocCL StrToCharList(__FILE__ ":" BOOST_PP_STRINGIZE(__LINE__) "#" BOOST_PP_STRINGIZE(__COUNTER__))
-#define FreshType(X) Fresh{pushContext(Context{},LocCL), X}
-#define RootFrame(name) auto name = makeContext(LocCL); using Context = decltype(name)
-#define FreshFrame(name) auto name = pushContext(ParentContext{},LocCL); using Context = decltype(name)
+#define UNIQUE detail::Unique<__LINE__, __COUNTER__>
+#define FreshType(X) Fresh{pushContext(Context{},UNIQUE{}), X}
+#define RootFrame(name) auto name = makeContext(UNIQUE{}); using Context = decltype(name)
+#define FreshFrame(name) auto name = pushContext(ParentContext{},UNIQUE{}); using Context = decltype(name)
 
-#define RefreshType(X) Fresh{pushContext(Context{},LocCL), X.v}
+#define RefreshType(X) Fresh{pushContext(Context{},UNIQUE{}), X.v}
 
 template<typename UniqueContext, typename V>
 class Return {
@@ -245,7 +239,7 @@ auto makeReturn(ParentContext) {
 	return Return<Context, V_>{};
 }
 
-#define MakeReturn(V) makeReturn<V>(pushContext(Context{},LocCL))
+#define MakeReturn(V) makeReturn<V>(pushContext(Context{},UNIQUE{}))
 
 
 namespace algebra {
@@ -266,7 +260,12 @@ namespace algebra {
 	
 	template<template<class L, class R> class Deriving, class L, class R>
 	struct commutative<Deriving<L,R>> {
-		constexpr operator Deriving<R,L> () const { return Deriving<R,L> {}; };
+		template<class Validator>
+		constexpr Deriving<R,L> operator()(Validator) const {
+			static_assert(CanValidate<Validator, L>::value && CanValidate<Validator, R>::value,
+						  "Validator must share root with terms and terms must not contain recursion");
+			return Deriving<R,L> {};
+		};
 	protected:
 		commutative() = default;
 	};
@@ -285,10 +284,18 @@ namespace algebra {
 	
 	template<template<class L, class R> class Deriving, class L, class R>
 	struct transitive<Deriving<L,R>> {
-		template<typename LL>
-		constexpr Deriving<LL,R> operator()(Deriving<LL,L>) const { return Deriving<LL,R>{}; }
-		template<typename RR>
-		constexpr Deriving<L,RR> operator()(Deriving<R,RR>) const { return Deriving<L,RR>{}; }
+		template<class LL,class Validator>
+		constexpr Deriving<LL,R> operator()(Deriving<LL,L>,Validator) const {
+			static_assert(CanValidate<Validator, LL>::value && CanValidate<Validator, L>::value && CanValidate<Validator, R>::value,
+						  "Validator must share root with terms and terms must not contain recursion");
+			return Deriving<LL,R>{};
+		}
+		template<class RR,class Validator>
+		constexpr Deriving<L,RR> operator()(Deriving<R,RR>,Validator) const {
+			static_assert(CanValidate<Validator, L>::value && CanValidate<Validator, R>::value && CanValidate<Validator, RR>::value,
+						  "Validator must share root with terms and terms must not contain recursion");
+			return Deriving<L,RR>{};
+		}
 	protected:
 		constexpr transitive() = default;
 	};
@@ -305,10 +312,6 @@ namespace algebra {
 		Increasing
 	};
 	
-	template<class Deriving, Dir = Increasing>
-	struct strict_monotonic {
-		static_assert(is_unary_operator<Deriving>::value, "strict_monotonic subclass must be a binary operator");
-	};
 	
 	template<class Left, class Right>
 	struct less_than
@@ -322,7 +325,10 @@ namespace algebra {
 	public:
 		using transitive<Self>::operator();
 		
-		constexpr static std::optional<Self> apply(const Left& left, const Right& right) {
+		template<class Validator>
+		constexpr static std::optional<Self> apply(const Left& left, const Right& right, Validator) {
+			static_assert(CanValidate<Validator, Left>::value && CanValidate<Validator, Right>::value,
+						  "Validator must share root with terms and terms must not contain recursion");
 			if(left.v < right.v) {
 				return Self{};
 			} else {
@@ -343,9 +349,12 @@ namespace algebra {
 		constexpr equal_to() = default;
 	public:
 		using transitive<Self>::operator();
-		using commutative<Self>::operator equal_to<Right, Left>;
+		using commutative<Self>::operator();
 		
-		constexpr static std::optional<Self> apply(const Left& left, const Right& right) {
+		template<class Validator>
+		constexpr static std::optional<Self> apply(const Left& left, const Right& right, Validator) {
+			static_assert(CanValidate<Validator, Left>::value && CanValidate<Validator, Right>::value,
+						  "Validator must share root with terms and terms must not contain recursion");
 			if(left.v == right.v) {
 				return Self{};
 			} else {
@@ -354,24 +363,33 @@ namespace algebra {
 		}
 	};
 	
-	template<template<class A> class Deriving, class A, Dir dir>
-	struct strict_monotonic<Deriving<A>, dir> {
+	template<typename Left, typename Right>
+	using less_than_or_equal_to = std::variant<less_than<Left, Right>, equal_to<Left, Right>>;
+	
+	template<template<class ...As> class Deriving, class A, Dir dir = Increasing>
+	struct strict_monotonic {
 		template<class LT> using LTTransform = less_than<Deriving<LT>,Deriving<A>>;
 		template<class GT> using GTTransform = less_than<Deriving<A>,Deriving<GT>>;
 		template<template<class B> class T, template<class Bp> class Tp, class B>
 		using Swap = std::conditional<dir == Increasing, T<B>, Tp<B>>;
 		static_assert((dir == Increasing) || (dir == Decreasing), "Incomplete case analysis");
 		
-		template<typename Eq>
-		constexpr equal_to<Deriving<A>,Deriving<Eq>> operator()(equal_to<A,Eq>) const {
+		template<class Eq, class Validator>
+		constexpr equal_to<Deriving<A>,Deriving<Eq>> operator()(equal_to<A,Eq>, Validator) const {
+			static_assert(CanValidate<Validator, A>::value && CanValidate<Validator, Eq>::value,
+						  "Validator must share root with terms and terms must not contain recursion");
 			return equal_to<Deriving<A>,Deriving<Eq>>{};
 		}
-		template<typename LT>
-		constexpr Swap<LTTransform,GTTransform,LT> operator()(less_than<LT,A>) const {
+		template<class LT, class Validator>
+		constexpr Swap<LTTransform,GTTransform,LT> operator()(less_than<LT,A>, Validator) const {
+			static_assert(CanValidate<Validator, LT>::value && CanValidate<Validator, A>::value,
+						  "Validator must share root with terms and terms must not contain recursion");
 			return {};
 		}
-		template<typename GT>
-		constexpr Swap<GTTransform,LTTransform,GT> operator()(less_than<A,GT>) const {
+		template<class GT, class Validator>
+		constexpr Swap<GTTransform,LTTransform,GT> operator()(less_than<A,GT>, Validator) const {
+			static_assert(CanValidate<Validator, A>::value && CanValidate<Validator, GT>::value,
+						  "Validator must share root with terms and terms must not contain recursion");
 			return {};
 		}
 	protected:
@@ -381,6 +399,7 @@ namespace algebra {
 	template<class Context, class Left, class Right>
 	struct PositiveDivision;
 	
+	/*
 	template<template<class ...Args> class Functor, class Arg>
 	struct CurryL {
 		template<typename ...RemArgs> using type = Functor<Arg, RemArgs...>;
@@ -390,25 +409,98 @@ namespace algebra {
 	struct CurryR {
 		template<typename ...RemArgs> using type = Functor<RemArgs..., Arg>;
 	};
+	*/
+	
+	template <template <typename...> class OP, typename...Ts>
+	struct tbind;
+	
+	template<size_t N>
+	struct _TP {};
+	
+	namespace _detail {
+		template <template <typename...> class OP, typename PARAMS, typename...Ts>
+		struct tbind_impl;
+		
+		template <template <typename...> class OP, typename...Ss>
+		struct tbind_impl<OP, std::tuple<Ss...>>
+		{
+			template<typename...Us>
+			using ttype = OP<Ss...>;
+		};
+		
+		template <template <typename...> class OP, typename T, typename...Ts, typename...Ss>
+		struct tbind_impl<OP, std::tuple<Ss...>, T, Ts...>
+		{
+			template<typename...Us>
+			using ttype = typename tbind_impl<
+			OP
+			, std::tuple<Ss..., T>
+			, Ts...
+			>::template ttype<Us...>;
+		};
+		
+		template <template <typename...> class OP, size_t I, typename ...Ts, typename...Ss>
+		struct tbind_impl<OP, std::tuple<Ss...>, _TP<I>, Ts...>
+		{
+			template<typename...Us>
+			using ttype = typename tbind_impl<
+			OP
+			, typename std::tuple<
+			Ss...
+			, typename std::tuple_element<
+			I
+			, typename std::tuple<Us...>
+			>::type
+			>
+			, Ts...
+			>::template ttype<Us...>;
+		};
+	}
+	
+	template <template <typename...> class OP, typename...Ts>
+	struct tbind : _detail::tbind_impl<OP, std::tuple<>, Ts...>
+	{};
+
+	
+	
+	template<typename Val>
+	using Positive = std::pair<Val, less_than<Constant<0>, Val>>;
 	
 	template<class Context, class Left, class Right>
 	struct PositiveDivision
-	: strict_monotonic<typename CurryR<PositiveDivision, Right>::template type<Left>, Increasing>
-	, strict_monotonic<typename CurryL<PositiveDivision, Left>::template type<Right>, Decreasing> {
+	: strict_monotonic<tbind<PositiveDivision, Context, _TP<0>, Right>::template ttype, Left, Increasing>
+	, strict_monotonic<tbind<PositiveDivision, Context, Left, _TP<0>>::template ttype, Right, Decreasing> {
 		static_assert(detail::is_context_tag<Context>::value, "Context must be context");
 		static_assert(((is_constant<Left>::value || is_fresh<Left>::value) &&
 					   (is_constant<Right>::value || is_fresh<Right>::value)),
 					  "Can only divide fresh variables and constants");
-		using strict_monotonic<typename CurryR<PositiveDivision, Right>::template type<Left>, Increasing>::operator();
-		using strict_monotonic<typename CurryL<PositiveDivision, Left>::template type<Right>, Decreasing>::operator();
+		using strict_monotonic<tbind<PositiveDivision, Context, _TP<0>, Right>::template ttype, Left, Increasing>::operator();
+		using strict_monotonic<tbind<PositiveDivision, Context, Left, _TP<0>>::template ttype, Right, Decreasing>::operator();
 		
-		//FreshType(result);
+	private:
+		using UniqueContext = typename detail::PushContext<Context, UNIQUE>::type;
+	public:
+		Fresh<UniqueContext, decltype(std::declval<Left>().v / std::declval<Right>().v)> result;
 		
-		//constexpr PositiveDivision(Context, less_than<Constant<0>, Left>,less_than<Constant<0>, Right>)
-		//: result() {}
+		constexpr PositiveDivision(Context, Positive<Left> left, Positive<Right> right)
+		: result{UniqueContext{}, left.first.v / right.first.v} {}
+		
+		template<class Validator>
+		constexpr equal_to<decltype(result), Left> operator()(equal_to<Right, Constant<1>>, Validator) const {
+			return {};
+		}
+		
+		template<class Validator>
+		constexpr less_than<decltype(result), Left> operator()(less_than<Constant<1>, Right>, Validator) const {
+			return {};
+		}
+		
+		// I feel like we should be able to prove this one in terms of monotonicity ...
+		template<class Validator>
+		constexpr less_than<Constant<0>, decltype(result)> operator()(less_than_or_equal_to<Right, Left>, Validator) const {
+			return {};
+		}
 	};
-	
-	using TwoThirds = PositiveDivision<ContextTag<CharList<'D','u','m','m','y','\0'>>, Constant<2>, Constant<3>>;
 }
 
 template<class ParentContext, class VarContext>
@@ -422,16 +514,27 @@ auto bad() {
 	return FreshType(2);
 }
 
+using Zero = Constant<0>;
+using One = Constant<1>;
+using Two = Constant<2>;
 template<class ParentContext, class ReturnContext, class VarContext>
-constexpr Fresh<ReturnContext, int> log2(Return<ReturnContext, int> rGen, Fresh<VarContext, int> var, algebra::less_than<Constant<0>,decltype(var)> gt0) {
+constexpr Fresh<ReturnContext, int> log2(Return<ReturnContext, int> rGen,
+										 Fresh<VarContext, int> var,
+										 algebra::less_than<Zero, decltype(var)> gt0) {
 	FreshFrame(context);
-	if(auto maybeProof = algebra::less_than<Constant<1>,decltype(var)>::apply(Constant<1>{}, var);
+	if(auto maybeProof = algebra::less_than<One,decltype(var)>::apply(One{}, var, context);
 	   std::nullopt == maybeProof) {
 		return std::move(rGen)(0);
 	} else {
+		using TwoIsPositive = algebra::less_than<Zero,Two>;
+		constexpr TwoIsPositive twP{*TwoIsPositive::apply(Constant<0>{}, Constant<2>{}, context)};
+		
 		auto gt1 = *maybeProof;
-		auto nextArg = FreshType(var.v / 2);
-		//auto nextReturn = log2<Context>(MakeReturn(int), nextArg, gt0);
+		using Divide = algebra::PositiveDivision<Context, decltype(var), Constant<2>>;
+		auto nextArg = Divide(context,
+							  algebra::Positive<decltype(var)>{var, gt0},
+							  algebra::Positive<Constant<2>>{ Constant<2>{}, twP });
+		auto nextReturn = log2<Context>(MakeReturn(int), nextArg.result, nextArg());
 		//return std::move(rGen)(1 + nextReturn.v);
 		return std::move(rGen)(0);
 	}
@@ -451,7 +554,7 @@ int main(int argc, const char* argv[]) {
 	//std::cout << DEPEND_EQUIV(bad(), bad()) << std::endl;
 	std::cout << DEPEND_EQUIV(RefreshType(bad()), RefreshType(bad())) << std::endl;
 	auto v32 = FreshType(32);
-	if(auto proofPositive = algebra::less_than<Constant<0>,decltype(v32)>::apply(Constant<0>{}, v32);
+	if(auto proofPositive = algebra::less_than<Constant<0>,decltype(v32)>::apply(Constant<0>{}, v32, context);
 	   std::nullopt == proofPositive) {
 		auto logOf32 = log2<Context>(MakeReturn(int),v32,*proofPositive);
 		std::cout << "log_2(" << v32 << ") = " << logOf32 << std::endl;
